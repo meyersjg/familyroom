4b9a8134d2a96b10d39d4b1a33c2c17ffdc8c663
John Meyers <meyersjg@miamioh.edu>
2013-12-13 08:27:34 -0600

    Fixed email for production

--- config/environment.rb
+++ config/environment.rb
@@ -2,17 +2,26 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-FamilyRoom::Application.initialize!
 
-CREDENTIALS = YAML.load_file(Rails.root.join('config', 'email.yml'))
+
+if Rails.env.development?
+CREDENTIALS = YAML.load_file(Rails.root.join('config','email.yml'))
+end
+
+
+EMAIL = Rails.env.development? ? CREDENTIALS['username'] : ENV['EMAIL']
+PASSWORD = Rails.env.development? ? CREDENTIALS['password'] : ENV['PASSWORD']
+
 
 
 ActionMailer::Base.smtp_settings = {
-  user_name: CREDENTIALS["email"],
-  password: CREDENTIALS["password"],
+  user_name: EMAIL,
+  password: PASSWORD,
   address: 'smtp.gmail.com',
   port: 587,
-  domain: 'heroku.com',
+  # domain: 'heroku.com',
   authentication: 'plain',
   enable_starttls_auto: true
    }
+
+FamilyRoom::Application.initialize!
